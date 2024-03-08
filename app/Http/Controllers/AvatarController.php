4dc49e529859d2d1a1c3cfff1bc8c69c1f1e90cd
azra <azrayazidalkautsar@gmail.com>
2024-03-08 15:02:57 +0700

    update: avatar service

--- app/Http/Controllers/AvatarController.php
+++ app/Http/Controllers/AvatarController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Avatar;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response; 
 use Illuminate\Http\JsonResponse;
 use App\Http\Requests\AvatarRequest;
-use App\Models\Avatar;
 use App\Http\Resources\AvatarResource;
 use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 
@@ -50,52 +50,11 @@ class AvatarController extends Controller
         return (new AvatarResource($avatar))->response()->setStatusCode(201);
     }
 
-        public function getAllAvatar()
+    public function getAllAvatar()
     {
         $avatars = Avatar::all();
         return response()->json(['avatars' => $avatars], 200);
     }
-
-    // public function updateAvatar(Avatar $avatar, Request $request)
-    // {
-    //         $this->validate($request, [
-    //         'image' => 'required|image|mimes:jpeg,jpg,png,|max:2048',
-    //         'avatar_name' => 'required|string',
-    //         'price' => 'required|numeric',
-    //         'status' => 'required|string',
-    //         ]);
-
-    //         cloudinary()->upload(
-    //             $request->file('image')->getRealPath(), [
-    //             'transformation' => [
-    //                 'gravity' => 'auto',
-    //                 'width' => 300,
-    //                 'height' => 300,
-    //                 'crop' => 'crop'
-    //             ]
-    //         ])->getSecurePath();
-
-    //         $avatar = Avatar::find($id);
-    
-    //         $uploadedFile = $request->file('image'); 
-    //         $result = Cloudinary::upload($uploadedFile->getRealPath(), [
-    //         'folder' => 'teka_apps', 
-    //         'public_id' => 'image-' . time(),
-    //         'overwrite' =>  true,
-    //         ]);
-    
-    //         $image = $result->getSecurePath();
-
-    //         $avatar->update([
-    //             'image'         => $image,
-    //             'avatar_name'   => $request->avatar_name,
-    //             'price'         => $request->price,
-    //             'status'        => $request->status,
-    //         ]);
-
-    //         return (new AvatarResource($avatar))->response()->setStatusCode(201);
-    // }
-
 
     public function updateAvatar(Request $request, $id)
     {
@@ -133,9 +92,7 @@ class AvatarController extends Controller
         return (new AvatarResource($avatar))->response()->setStatusCode(201);
     }
 
-    
-
-        public function deleteAvatar(Request $request, $id)
+    public function deleteAvatar(Request $request, $id)
     {
         $avatar = Avatar::find($id);
 
